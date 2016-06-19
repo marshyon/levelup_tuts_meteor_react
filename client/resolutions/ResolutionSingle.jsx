11a7c22101010afa42d319768e7e6cb3c910c14d
Jon Brookes <marshyon@gmail.com>
2016-06-19 17:57:57 +0100

    added delete button and method call to toggle done status

--- client/resolutions/ResolutionSingle.jsx
+++ client/resolutions/ResolutionSingle.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 
 export default class ResolutionSingle extends Component {
+
+  toggleChecked() {
+    console.log(this);
+    Meteor.call("toggleResolution", this.props.resolution._id, this.props.resolution.complete);
+  }
+
+  deleteResolution() {
+    Meteor.call("deleteResolution", this.props.resolution._id);
+  }
+
+
   render() {
     return (
       <li>
-        {this.props.resolution.text} ::
-        {this.props.resolution.complete.toString()}
+        <input type="checkbox"
+          readOnly={true}
+          checked={this.props.resolution.complete}
+          onClick={this.toggleChecked.bind(this)} />
+        {this.props.resolution.text}
+        <button className="btn-cancel"
+          onClick={this.deleteResolution.bind(this)}>
+        &times;
+      </button>
       </li>
     )
   }
