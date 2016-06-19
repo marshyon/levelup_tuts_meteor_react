bcfc75b7eab56f619d157c50004c88fddb198816
Jon Brookes <marshyon@gmail.com>
2016-06-19 15:16:40 +0100

    initial commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello World :) !</h1>
+          <h2>thank you and goodnight</h2>
+        </div>
+      )
+    }
+}
