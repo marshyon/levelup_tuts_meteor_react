c9bc1d79c6ef3999dbb1c889ea2f308b02a7ad4b
Jon Brookes <marshyon@gmail.com>
2016-06-19 16:28:37 +0100

    added components ResolutionsForm and ResolutionSingle, latter with a 'prop' passed

--- App.jsx
+++ App.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import ResolutionsForm from './ResolutionsForm.jsx';
+import ResolutionSingle from './ResolutionSingle.jsx';
 
 Resolutions = new Mongo.Collection("resolutions");
 
-export default class App extends React.Component {
-    addResolution(event) {
-      event.preventDefault();
-      var text= this.refs.resolution.value.trim();
-      Resolutions.insert({
-          text: text,
-          complete: false,
-          createdAt: new Date()
-      });
-      this.refs.resolution.value = "";
-      //console.log(text)
+export default class App extends TrackerReact(React.Component) {
 
+    resolutions() {
+      return Resolutions.find().fetch();
     }
+
+
     render() {
+      let res = this.resolutions();
+      if(res.length < 1) {
+        return(<div>loading ...</div>)
+      }
+      console.log(this.resolutions());
       return (
         <div>
           <h1>My Resolutions</h1>
-          <form className="new-resultion" onSubmit={this.addResolution.bind(this)}>
-            <input
-              type="text"
-              ref="resolution"
-              placeholder="Finish React Meteor Series" />
-          </form>
+          <ResolutionsForm />
+
+          <ul>
+            <ResolutionSingle resolution={res[0]} />
+          </ul>
         </div>
       )
     }
