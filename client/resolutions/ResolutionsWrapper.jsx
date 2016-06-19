b342a9684e842da42496f56e17fc576b8e07e254
Jon Brookes <marshyon@gmail.com>
2016-06-19 17:04:32 +0100

    added iterator by means of javascript 'map' functionality and es6 function syntax

--- client/resolutions/ResolutionsWrapper.jsx
+++ client/resolutions/ResolutionsWrapper.jsx
@@ -22,9 +22,10 @@ export default class ResolutionsWrapper extends TrackerReact(React.Component) {
         <div>
           <h1>My Resolutions</h1>
           <ResolutionsForm />
-
-          <ul>
-            <ResolutionSingle resolution={res[0]} />
+          <ul className="resolutions">
+            {this.resolutions().map( (resolution) => {
+              return <ResolutionSingle key={resolution._id} resolution={resolution} />
+            })}
           </ul>
         </div>
       )
